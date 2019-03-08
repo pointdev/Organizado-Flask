9b5518ba4074593bbd23df0bdfa75188e378488f
Luis Sierra <37555594+LGSierra@users.noreply.github.com>
2019-03-08 17:28:03 -0400

    update

---
+++ SQLtest.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
