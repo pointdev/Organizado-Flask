83f204d48b16e389fc1068810b46a1ad148b88cb
Luis Sierra <37555594+LGSierra@users.noreply.github.com>
2019-03-13 17:48:03 -0400

    added button

--- test.py
+++ test.py
@@ -13,37 +13,32 @@ app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return redirect('/crearEscuela')
 
-    if request.method == 'POST':
-        #Fetch form data
-        userDetails = request.form
-        name = userDetails['name']
-        email = userDetails['email']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)",(name, email))
-        mysql.connection.commit()
-        cur.close()
-        return redirect('/users')
-    return render_template('index.html')
+#@app.route('/', methods=['GET', 'POST'])
+#def index():
+#
+#    if request.method == 'POST':
+#        #Fetch form data
+#        userDetails = request.form
+#        name = userDetails['name']
+#        email = userDetails['email']
+#        cur = mysql.connection.cursor()
+#        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)",(name, email))
+#        mysql.connection.commit()
+#        cur.close()
+#        return redirect('/users')
+#    return render_template('index.html')
 
-@app.route('/users')
-def users():
-    cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM users")
-    if resultValue > 0:
-        userDetails = cur.fetchall()
-        return render_template('users.html',userDetails=userDetails)
-
-#CODIGO PARTICIPACION===============================================
-@app.route('/codigoParticipacion')
-def codigoParticipacion():
-    cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM codigoParticipacion")
-    if resultValue > 0:
-        codigoDetails = cur.fetchall()
-        return render_template('codigoParticipacion.html',codigoDetails=codigoDetails)
+#@app.route('/users')
+#def users():
+#    cur = mysql.connection.cursor()
+#    resultValue = cur.execute("SELECT * FROM users")
+#    if resultValue > 0:
+#        userDetails = cur.fetchall()
+#        return render_template('users.html',userDetails=userDetails)
 
 #ESCUELA==============================================================
 @app.route('/crearEscuela', methods=['GET', 'POST'])
@@ -71,6 +66,20 @@ def escuelas():
     if resultValue > 0:
         escuelaDetails = cur.fetchall()
         return render_template('escuelas.html',escuelaDetails=escuelaDetails)
+
+
+
+#CODIGO PARTICIPACION===============================================
+@app.route('/codigoParticipacion')
+def codigoParticipacion():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM codigoParticipacion")
+    if resultValue > 0:
+        codigoDetails = cur.fetchall()
+        return render_template('codigoParticipacion.html',codigoDetails=codigoDetails)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
