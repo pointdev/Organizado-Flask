08238495e691a549699811585ac7f05e93f77ab7
Luis Sierra <37555594+LGSierra@users.noreply.github.com>
2019-04-02 06:47:49 -0400

    commented out yaml
    no more yaml

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
-import yaml
+#import yaml
 
 app = Flask(__name__)
 
 #configure db
-db = yaml.load(open('db.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
+#db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flaskapp'
 
 mysql = MySQL(app)
 
