1da6741acfe336840cbee7bbc6af1fc5b7bafbaf
Luis Sierra <37555594+LGSierra@users.noreply.github.com>
2019-04-04 16:30:24 -0400

    Some changes to make the app work with JawsDB on Heroku

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 #configure db
 #db = yaml.load(open('db.yaml'))
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_HOST'] = 'localfnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.comhost'
+app.config['MYSQL_USER'] = 'ophmvxggd70qi4hx'
+app.config['MYSQL_PASSWORD'] = 'lxv5byb85vydqsnu'
+app.config['MYSQL_DB'] = 'JAWSDB'
 
 mysql = MySQL(app)
 
