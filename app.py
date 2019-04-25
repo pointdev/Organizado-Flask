fdd78ce352e79d7bb271a78b1541cc0c53387467
AnnamaryC <annamary.cartagena@hotmail.com>
2019-04-25 14:32:35 -0400

    error code
    applying error codes, not functional

--- app.py
+++ app.py
@@ -144,3 +144,18 @@ def home():
 #=============================================================================
 if __name__ == '__main__':
     app.run(debug=True)
+
+# RENDER ERROR PAGES =========================================================
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+  def create_app(config_filename):
+    app = Flask(__name__)
+    app.register_error_handler(404, page_not_found)
+    return app
