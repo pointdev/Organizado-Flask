6c9d68a3cb872113f169d8e30dd32369879f6797
AnnamaryC <annamary.cartagena@hotmail.com>
2019-04-26 15:35:45 -0400

    Error handlers

--- app.py
+++ app.py
@@ -127,10 +127,22 @@ if __name__ == '__main__':
 @app.errorhandler(404)
 def page_not_found(e):
         # note that we set the 404 status explicitly
-        return render_template('404.html'), 404
+        return render_template('errors/404.html'), 404
         
 
 @app.errorhandler(500)
 def internal_server_error(e):
         # note that we set the 404 status explicitly
         return render_template('errors/500.html'), 500
+
+
+@app.errorhandler(403)
+def internal_server_error(e):
+        # note that we set the 404 status explicitly
+        return render_template('errors/403.html'), 403
+
+
+@app.errorhandler(410)
+def internal_server_error(e):
+        # note that we set the 404 status explicitly
+        return render_template('errors/510.html'), 410
